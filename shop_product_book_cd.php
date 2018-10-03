105e2759b6b446c781a5d94f12398d0a2b683933
Vladimir Kashin <kkvvnn89@gmail.com>
2018-10-03 22:08:31 +0500

    Add "getSummaryLine()" method

--- shop_product_book_cd.php
+++ shop_product_book_cd.php
@@ -3,20 +3,22 @@
 Header("Content-Type:text/html; charset=UTF-8");
 
 class ShopProduct {
-	public $numPages          = 0;
-	public $playLength        = 0;
 	public $title 		  = "Стандартный товар";
 	public $producerFirstName = "Имя автора";
 	public $producerMainName  = "Фамилия автора";
 	public $price 		  = 0;
-
+	public $type              = "unknown_type";
+	public $numPages          = 0;
+	public $playLength        = 0;
+	
 	public function __construct( $title, $firstName, $mainName, 
-				     $price, $numPages=0, $playLength=0 ) 
+				     $price, $type, $numPages=0, $playLength=0 ) 
 	{
 		$this->title             = $title;
 		$this->producerMainName  = $mainName;
 		$this->producerFirstName = $firstName;
 		$this->price             = $price;
+		$this->type              = $type;
 		$this->numPages          = $numPages;
 		$this->playLength        = $playLength;
 	}
@@ -31,6 +33,20 @@ class ShopProduct {
 		return $this->playLength;
 	}
 	
+	public function getSummaryLine()
+	{
+		$base  = "{$this->title} ( {$this->producerMainName}, ";
+		$base .= "{$this->producerFirstName} )";
+
+		if ($this->type == 'book') {
+			$base .= ": {$this->numPages} стр.";
+		} else if ($this->type == 'cd') {
+			$base .= ": Время звучания - {$this->playLength}";
+		}
+
+		return $base;
+	}
+
 	public function getProducer() 
 	{
 		return "{$this->producerFirstName} "
